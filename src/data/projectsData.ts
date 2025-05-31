de82c4a9602463cd9f9ebeb40092103b2f511f41
Chirag Sharma <chiragnoida3345@gmail.com>
2025-05-31 16:43:25 +0530

    Added Images

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
     id: 1,
     title: 'Codivio: A Video Interview Platform',
     description: 'a full-stack video interview platform with live coding IDE, enabling real-time collaboration, problem-solving, and evaluation between interviewers and candidates.',
-    image: 'public/Codivio.png',
+    image: '/Codivio.png',
     techStack: ['Next.js', 'React.js' , 'Typescript', 'Stream', 'Convex', 'Tailwind'],
     demoLink: 'https://codivio.vercel.app',
     githubLink: 'https://github.com/chiraagsharma24/Codivio'
@@ -14,7 +14,7 @@ export const projects: Project[] = [
     id: 2,
     title: 'NeuroWealth: AI Powered Finance Platform',
     description: 'AI-powered finance platform that lets you manage money, track budgets, analyze spending, AI receipt scanning and get smart insights with all from one sleek, intelligent dashboard.',
-    image: 'public/NeuroWealth.png',
+    image: '/NeuroWealth.png',
     techStack: ['Next.js', 'React.js', 'Javascript', 'Prisma', 'Inngest','Arcjet','Gemini-API'],
     demoLink: 'https://neurowealth-green.vercel.app',
     githubLink: 'https://github.com/chiraagsharma24/NeuroWealth'
@@ -23,7 +23,7 @@ export const projects: Project[] = [
     id: 3,
     title: 'Sprintly: Full Stack Project Management Platform',
     description: 'Helps startups, freelancers, and teams break down complex projects into sprints. Manage tasks and track progress easily with a clean, visual Kanban interface built for clarity and speed.',
-    image: 'public/Sprintly.png',
+    image: '/Sprintly.png',
     techStack: ['Next.js', 'React.js', 'Javascript', 'Prisma','Tailwind'],
     demoLink: 'https://sprintly-opal.vercel.app',
     githubLink: 'https://github.com/chiraagsharma24/Sprintly'
@@ -32,7 +32,7 @@ export const projects: Project[] = [
     id: 4,
     title: 'UpNext: AI-powered Career Coach',
     description: 'A platform which provides everything you need to build a great tech career from expert guidance to job tracking and resume optimization.',
-    image: 'public/UpNext.png',
+    image: '/UpNext.png',
     techStack: ['Next.js', 'React.js', 'Inngest','Gemini-API'],
     demoLink: 'https://up-next-vert.vercel.app',
     githubLink: 'https://github.com/chiraagsharma24/Up-Next'
@@ -41,7 +41,7 @@ export const projects: Project[] = [
     id: 5,
     title: 'Stormie: Smart Weather Forecast App',
     description: 'A platform that shows real-time temperature, humidity, pressure, and 5-day forecasts from anywhere in the world, right on your screen.',
-    image: 'public/Stormie.png',
+    image: '/Stormie.png',
     techStack: ['React', 'OpenWeather API', 'Recharts'],
     demoLink: 'https://stormie.vercel.app',
     githubLink: 'https://github.com/chiraagsharma24/Stormie'
@@ -50,7 +50,7 @@ export const projects: Project[] = [
     id: 6,
     title: 'ReadYourWay: Article Summarizer',
     description: 'Chrome extension that summarizes any article in your preferred style: brief, detailed, or bullet points, making reading faster and smarter.',
-    image: 'ReadYourWay.png',
+    image: '/ReadYourWay.png',
     techStack: ['Javascript', 'CSS', 'Html'],
     demoLink: './#projects',
     githubLink: 'https://github.com/chiraagsharma24/ReadYourWay'
